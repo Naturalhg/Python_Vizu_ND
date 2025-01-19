1edce16dd329856aaa2b1a02b6e542bbf0cf2995
Samy-Meliani <samymeliani24@gmail.com>
2025-01-19 15:02:48 +0100

    recorrection encore

--- map.py
+++ map.py
@@ -13,12 +13,17 @@ class NaturalDisasterMap:
         map_bounds (list) : Coordonnées max de la carte.
         m (folium.Map) : Carte folium pour les visualisations.
     """
+
+
+
     def __init__(self, disaster_data_path):
         """
         Initialise la carte avec les données des catastrophes naturelles enrichies.
 
         :param disaster_data_path: Chemin vers le fichier CSV contenant les données enrichies.
         """
+        self.genCoords()
+
         self.df = pd.read_csv(disaster_data_path)
         self.df = self.df.dropna(subset=['latitude', 'longitude'])
 
@@ -77,4 +82,22 @@ class NaturalDisasterMap:
         Retourne la carte avec les marqueurs ajoutés.
         """
         return self.m
+
+    def genCoords(self):
+        # Charger les fichiers
+        disasters_file = './data/All_Natural_Disasters.csv'
+        coordinates_file = './data/country-coordinates-world.csv'
+
+        # Lire les données en DataFrames
+        disasters_df = pd.read_csv(disasters_file)
+        coordinates_df = pd.read_csv(coordinates_file)
+
+
+        # Fusionner les deux fichiers sur la colonne 'Country'
+        # (adapter le nom de la colonne si nécessaire dans vos fichiers)
+        merged_df = pd.merge(disasters_df, coordinates_df, how='left', left_on='Country', right_on='Country')
+
+        # Sauvegarder dans un nouveau fichier
+        output_file = './data/All_Natural_Disasters_with_Coordinates.csv'
+        merged_df.to_csv(output_file, index=False)
 
